304bcd04c50397293ae81c22c9870773fc2062b1
vansh <vansha10@gmail.com>
2020-03-10 00:11:18 +0530

    Added ViewModel

--- app/src/main/java/com/o/notetaker/MainActivity.java
+++ app/src/main/java/com/o/notetaker/MainActivity.java
@@ -1,14 +1,32 @@
 package com.o.notetaker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewModelProvider.AndroidViewModelFactory viewModelFactory = new ViewModelProvider.
+                AndroidViewModelFactory(getApplication());
+        noteViewModel = new ViewModelProvider(this, viewModelFactory).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update recycler view
+                Toast.makeText(MainActivity.this, notes.get(0).getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
